a326e1ea2e686f6ce835e1f5876165882ac5d1aa
Vincent Ambo <tazjin@gmail.com>
2018-04-14 16:40:56 +0200

    feat: License project under GPLv3

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2018  Vincent Ambo <mail@tazj.in>
+//
+// Converse is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+
 //! This module defines custom error types using the `failure`-crate.
 //! Links to foreign error types (such as database connection errors)
 //! are established in a similar way as was tradition in
